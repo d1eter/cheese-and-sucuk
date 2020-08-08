8b78193dcc6333b2bd155e2a08cd456179f99be3
Dieter Pankratz <pankratz117@gmail.com>
2020-08-09 00:00:13 +0200

    add barebones for app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+// variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const menuDOM = document.querySelector('.menu');
+
+// cart 
+let cart = [];
+
+// getting the menu items
+class Menu {
+
+}
+
+// display menu
+class UI {
+
+}
+
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const menu = new Menu();
+})
